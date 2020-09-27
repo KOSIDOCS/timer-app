9bfcac9302178a3aec6bd6a9c0bf9aaee8a20b08
Kosidocs <kelvinonaru@gmail.com>
2020-09-28 02:54:07 +0400

    added list of timers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import reducers from './reducers';
 import logo from './logo.svg';
 import './App.css';
+import NewTimer from './components/new-timer'
+import ListTimers from './list-timers'
 
 const store = createStore(reducers);
 
@@ -12,19 +14,11 @@ function App() {
     <Provider store={store}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
       </header>
+      <NewTimer />
+
+      <ListTimers />
     </div>
     </Provider>
   );
