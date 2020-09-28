a20d0e94c6deeab311845d44b501873bbeed99fc
Kosidocs <kelvinonaru@gmail.com>
2020-09-28 05:06:16 +0400

    styled timer view

--- src/timer-view.js
+++ src/timer-view.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatTime, resetTime } from './utils/index'
+import './components/timer-view.css'
 
 // Import our toggleTimer action
 import { toggleTimer, deleteTimer } from './actions/index'
@@ -16,31 +17,32 @@ class TimerView extends Component {
 
         // Extract these specific props to use in the component
         const { index, toggleTimer, timer} = this.props
+        const buttonClass = timer.isRunning ? "stop" : "start";
         return (
-            <div>
-        <h2>{timer.name}</h2>
-        <h1>{formatTime(timer.time)}</h1>
+            <div className='timer-view'>
+        <h2 className='timer-view-name__h2'>{timer.name}</h2>
+        <h1 className='timer-view-name___h1'>{formatTime(timer.time)}</h1>
         <button
             // This calls our toggleTimer action on the specific timer (specified by the index)
             onClick={(e) => {
                 toggleTimer(index)
-            }}>
+            }} className={`timer-view__button timer-view__button--${buttonClass}`}>
              {/* Text of the button is determined by if the timer is running or not */}
             {timer.isRunning ? "Stop" : "Start"}
         </button>
 
-        <button onClick={(e) => {
+        {/* <button onClick={(e) => {
             deleteTimer(index)
-        }}>
+        }} className='timer-view-name__button'>
              Delete
         </button>
 
         <button onClick={(e) => {
             resetTime(timer.time)
         }}
-        >
+        className='timer-view-name__button'>
             reset
-        </button>
+        </button> */}
       </div>
         )
     }
